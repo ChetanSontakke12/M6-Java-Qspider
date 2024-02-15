1ddc98ad9870009dc41981c371423ce47f8a3f9d
ChetanSontakke12 <158994962+ChetanSontakke12@users.noreply.github.com>
2024-02-15 13:16:33 +0530

    Add files via upload

---
+++ Reletional_Example1.java
@@ -0,0 +1,20 @@
+class Reletional_Example1
+{
+public static void main(String []re1)
+{
+int age1=25;
+int age2=80;
+int price1=56;
+int price2=58;
+int distance1=15;
+int distance2=15;
+
+//condition cheak the L.H.S<R.H.S in boolean result
+
+System.out.println("L.H.S is grater than R.H.S : " +(age1<age2));
+System.out.println("L.H.S is grater than R.H.S : " +(price1<price2));
+System.out.println("L.H.S is grater than R.H.S : " +(distance1<distance2));
+
+}
+
+}
